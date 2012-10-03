0c838411c1808e133a0d8bf24a648253ed50c3b0
EliezerCruz <eliezer1985@hotmail.com>
2012-10-03 12:41:55 +0100

    Declaracion del metodo computerplay

--- ppt.rb
+++ ppt.rb
@@ -40,3 +40,28 @@ def computerPlay()
     end
     @beats[humans_choice]
   end
+def play
+    loop do
+      h = humanPlay
+      c = computerPlay
+      print "H: #{h}, C: #{c} => "
+ 
+      # only update the human player's history after the computer has chosen
+      @plays[h] += 1
+ 
+      if h == c
+        puts "draw"
+      elsif h == @beats[c]
+        puts "Human wins"
+        @score[0] += 1
+      else
+        puts "Computer wins"
+        @score[1] += 1
+      end
+      puts "score: human=#{@score[0]}, computer=#{@score[1]}"
+    end
+  end
+end
+ 
+game = RockPaperScissorsGame.new
+
