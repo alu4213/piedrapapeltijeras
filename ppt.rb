d59795586e2d80139f5166235fac653c906468f1
EliezerCruz <eliezer1985@hotmail.com>
2012-10-03 12:40:19 +0100

    Declaracion del metodo humanplay

--- ppt.rb
+++ ppt.rb
@@ -10,3 +10,18 @@ Class RockPaperScissorgame
 
     play
   end
+def humanPlay()
+    answer = nil
+    loop do
+      print "\nYour choice: #@pieces? "
+      answer = STDIN.gets.strip.downcase
+      next if answer.empty?
+      if idx = @pieces.find_index {|piece| piece.match(/^#{answer}/)}
+        answer = @pieces[idx]
+        break
+      else
+        puts "invalid answer, try again"
+      end
+    end
+    answer
+  end
